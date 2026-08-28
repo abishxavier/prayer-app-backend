99cfccf9d3a1b01939025af07b17be6bf0441e1c
abishxavier <abishxavier2002@gmail.com>
2026-08-28 19:27:45 +0530

    Fix message serialization, persistent local caching, and 1-hour call limit

--- static/flutter_bootstrap.js
+++ static/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "318687625" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3740829385" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
